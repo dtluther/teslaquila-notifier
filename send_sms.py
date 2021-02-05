0d4558171f8772efaf05dc8543469e8235d82e1a
Dillon Luther <dillon.luther@gmail.com>
2021-02-05 14:33:30 -0800

    dotenv

---
+++ send_sms.py
@@ -0,0 +1,17 @@
+from dotenv import dotenv_values
+from twilio.rest import Client
+
+config = dotenv_values("twilio.env")
+
+account_sid = config['TWILIO_ACCOUNT_SID']
+auth_token = condig['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15017122661',
+                     to='+15558675310'
+                 )
+
+print(message.sid)
