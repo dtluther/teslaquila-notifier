c1ed4657b3a88b9ef526c2ccdc241171922b5c5c
Dillon Luther <dillon.luther@gmail.com>
2021-02-21 17:55:45 -0800

    updated based on challenged in notes.md

--- aws_notifier.py
+++ aws_notifier.py
@@ -30,29 +30,49 @@ def send_alert_message(src_number, dest_number, body):
     print(message.sid)
 
 seconds_in_day = 86400
-sleep_duration = 4
+sleep_duration = 10
 counts_in_day = seconds_in_day // sleep_duration
-counter = counts_in_day - 2
+counter = counts_in_day - 3
+allowed_on_site1 = False
+allowed_on_site2 = False
+
 while True:
+    if counter >= counts_in_day:
+        send_alert_message(from_number, to_number, "Daily update text. Teslaquila notifier is working.")
+        counter = 0
+        # reset trying after a day and see if it's valid again
+        allowed_on_site1 = True
+        allowed_on_site2 = True
+
     print(counter)
+    print(f"Can access site1: {site1}" if allowed_on_site1 else f"Denied access on site1: {site1}")
+    print(f"Can access site2: {site2}" if allowed_on_site2 else f"Denied access on site2: {site2}")
+    resp1, resp2 = None, None # clear the responses
     try:
-        resp1 = requests.get(site1)
-        resp2 = requests.get(site2)
+        if allowed_on_site1:
+            resp1 = requests.get(site1) # a 4XX response is falsey
+        if allowed_on_site2:
+            resp2 = requests.get(site2)
     except requests.ConnectionError as e:
         print("Couldn't connect to one of the sites, trying again")
-        sleep(3)
+        sleep(sleep_duration)
 
-    if resp1.text and "out of stock" not in resp1.text.lower():
-        print(f"IN STOCK at {site1}")
-        send_alert_message(from_number, to_number, f"Teslaquila IN STOCK! Get it now at {site1}!")
-    if resp2.text and "out of stock" not in resp2.text.lower():
-        print(f"IN STOCK at {site2}")
-        send_alert_message(from_number, to_number, f"Teslaquila IN STOCK! Get it now at {site2}!")
+    # can't just check for presence because a 4XX response is falsey
+    if type(resp1) is requests.models.Response: # we'll have a response if we were allowed_on_siteX is True
+        if resp1.status_code == 403:
+            print('DENIED')
+            allowed_on_site2 = False
+        elif "access denied" not in resp1.text.lower() and "out of stock" not in resp1.text.lower():
+            print(f"IN STOCK at {site2}")
+            send_alert_message(from_number, to_number, f"Teslaquila IN STOCK! Get it now at {site1}!")
+    if type(resp2) is requests.models.Response:
+        if resp2.status_code == 403:
+            print('DENIED')
+            allowed_on_site2 = False
+        elif "access denied" not in resp2.text.lower() and "out of stock" not in resp2.text.lower():
+            print(f"IN STOCK at {site2}")
+            send_alert_message(from_number, to_number, f"Teslaquila IN STOCK! Get it now at {site2}!")
     else:
         print("NO LUCK YET")
-    if counter >= counts_in_day:
-        send_alert_message(from_number, to_number, f"Daily update text. Teslaquila notifier is working.")
-        counter = 0
-
     sleep(sleep_duration)
     counter += 1
---
+++ notes.md
@@ -0,0 +1,7 @@
+# Challenges
+1. Got blocked from site2 (I think I was hitting it too much, so they forbid access from my IP)
+   1. Had to launch new instance
+   2. I changed the ping time to every 10 seconds instead
+      1. Also made a thing where I wouldn't try again for a day if I hit a 403 status code
+   
+
